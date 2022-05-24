ec332cc9c99a3e88663070f69a98cfdbd7ce2111
Victor D. Caselli <victor.porto@capgemini.com>
2022-05-23 21:13:56 -0300

    project created

---
+++ src/main/java/br/com/victorcaselli/frameworkchallenge/FrameworkChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.victorcaselli.frameworkchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victorcaselli/frameworkchallenge/FrameworkChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorcaselli.frameworkchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
